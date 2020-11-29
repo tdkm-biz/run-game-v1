2d35aee5fd92b0f2351ea986197b88e4c8ff43e6
tciuser <tciuser@tciusernoMacBook-Pro.local>
2020-11-29 14:03:34 +0900

    [add] webpack

---
+++ public/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log("hello webpack");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ydW4tZ2FtZS12MS8uL3NyYy9tYWluLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaGVsbG8gd2VicGFja1wiKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
