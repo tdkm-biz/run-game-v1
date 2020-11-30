e2a198db539fe04c85536c16cd06b2fa4cd3c2ff
tdkm-biz <tciuser@tciusernoMacBook-Pro.local>
2020-11-30 17:44:15 +0900

    [modify] webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
 const path = require("path");
-module.exports={
-    mode:'development',
-    devServer:{
-        open:true,
-        openPage:"index.html",
-        contentBase: path.join(__dirname, 'public'),
-        publicPath: path.resolve(__dirname, 'js'),
-        watchContentBase:true,
-        port:8080
+
+module.exports = {
+    mode: "development",
+    entry : "./src/main.js",
+    devServer: {
+        contentBase: "public",
+        open: true,
+        // 仮想JSの出力先
+        publicPath: '/js/'
     },
-    entry : './src/main.js',
-    output : {
-        filename: 'bundle.js',
-        path:path.resolve(__dirname, 'public/js')
+    output: {
+        // 出力ファイル
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'public/js')
     },
-    devtool : 'inline-source-map',
     performance: {
         maxEntrypointSize: 500000,
         maxAssetSize: 500000,
     },
-}
+};
